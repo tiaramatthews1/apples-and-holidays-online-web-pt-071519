d4b4b9f273a8779a09849ebeb4d822179e6a9e74
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-25 20:19:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,10 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+holiday_hash.each do |season, holiday|
+  
+  
+  puts 
 end
 
 def all_holidays_with_bbq(holiday_hash)
